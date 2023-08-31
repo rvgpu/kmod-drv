b6ce7aa238b37d106c546632831a469f957cc433
Qi Hangkong <qihangkong@outlook.com>
2023-08-31 14:33:46 +0800

    Revert "feat: use DRIVER_COMPUTE_ACCEL features not DRIVER_RENDER"
    This reverts commit 6d65365e1758ff2df96b6c19ecc2472c87e3041b.

--- rvgpu_drv.c
+++ rvgpu_drv.c
@@ -7,7 +7,6 @@
 #include <drm/drm_ioctl.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_module.h>
-#include <drm/drm_accel.h>
 
 #include "rvgpu.h"
 #include "rvgpu_dma_buf.h"
@@ -16,10 +15,19 @@
 static const struct drm_ioctl_desc rvgpu_ioctls[] = {
 };
 
-DEFINE_DRM_ACCEL_FOPS(rvgpu_driver_fops);
+static const struct file_operations rvgpu_driver_fops = {
+    .owner = THIS_MODULE,
+    .open = drm_open,
+    .release = drm_release,
+    .unlocked_ioctl = drm_ioctl,
+    .mmap = drm_gem_mmap,
+    .poll = drm_poll,
+    .read = drm_read,
+    .llseek = noop_llseek,
+};
 
 static const struct drm_driver rvgpu_drm_driver = {
-    .driver_features = DRIVER_GEM | DRIVER_COMPUTE_ACCEL,
+    .driver_features = DRIVER_GEM | DRIVER_RENDER,
     .open = rvgpu_driver_open,
     .postclose = rvgpu_driver_postclose,
     .lastclose = NULL,
